f5d1a3f66c454a4672e1000ad07a9be580a8d225
Schwaaprogramador <danieltoro.programacion@gmail.com>
2023-09-26 00:12:21 -0500

    notasgenerales

---
+++ NotasGenerales.py
@@ -0,0 +1,44 @@
+#----------------------------------------------
+#----------------------------------------------
+#    TUTORIAL SOLOPYTHON FULL COURSE DJANGO
+
+
+#----------------------------------------------
+#----------------------------------------------
+# Al inicial un proyecto siempre recordar Crear un ambiente virtual:
+#       Para instalarlo: pip install virtualenv [En la terminal]
+#       Para crear un ambiente: virtual env [En la terminal]
+#       Para Activarlo: navegar a la carpeta env/Scripts y ejecutar active
+
+
+#----------------------------------------------
+#----------------------------------------------
+#   Cuando se vincula el proyecto con github recordar crear el gitignore [python]
+#   Crear una branch para cada cambio del projecto.
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+
+
+#----------------------------------------------
+#----------------------------------------------
+#   Instalar dentro del ambiente virtual Django.
+#   [En la terminal] pip install -r requirements.txt
+#   Para crear un proeycto de django: django-admin startproject core . [En la terminal]
+#   Para iniciar el server : python manage.py runserver
+#
+#
+#
+#
+#
+#

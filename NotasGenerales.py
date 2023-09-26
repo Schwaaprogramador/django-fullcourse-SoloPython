20e6fa165632fc3af1fa772efe146745f228fe60
Schwaaprogramador <danieltoro.programacion@gmail.com>
2023-09-26 18:55:42 -0500

    app core

--- NotasGenerales.py
+++ NotasGenerales.py
@@ -15,18 +15,7 @@
 #----------------------------------------------
 #   Cuando se vincula el proyecto con github recordar crear el gitignore [python]
 #   Crear una branch para cada cambio del projecto.
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
+
 #
 
 
@@ -37,8 +26,34 @@
 #   Para crear un proeycto de django: django-admin startproject core . [En la terminal]
 #   Para iniciar el server : python manage.py runserver
 #
+#   Para hacer las migraciones:
+#   python manage.py migrate [En la terminal]
+#   
+#   
+#   Para crear un administrador:
+#   python manage.py createsuperuser
+#   admin admin123
+
+
+#----------------------------------------------
+#----------------------------------------------
+#   En el archivo Setting pusimos en comentarios la descripcion del cada uno de los codigos que viene por default.
+#   Al hacer las migraciones activamos las funcionalidades del setings.
+#       Esto es importante a la hora de trabajar con la DB y los modelos.
 #
 #
 #
 #
+
+
+#----------------------------------------------
+#----------------------------------------------
+#   Para crear una nueva views:
+#       crear una archivo views.py en el core:
+#           dentro de la clase, crear una fn() Get que retorna la request y el template
+#               def get(self, request, *args, **kwargs):
+#                return render(request, 'index.html', context)
 #
+#
+#   Para crer un template:
+#       creamos una carpeta template dentro del proyecto en si [fuera del core].
